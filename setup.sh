fad47a7f45b0797cfb61c2988f179d66fc29d10e
glalonde <glalonde@mit.edu>
2014-04-21 14:17:35 -0400

    fixed setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-add 6.824
-export GOPATH=$HOME/Documents/6824/final
+export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 alias gt="go test 2>/dev/null | egrep -v 'EOF|connection|broken'"
