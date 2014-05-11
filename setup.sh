a9a1a17a1e7c9a7e0a1948369562b012466968bb
Geoffrey Lalonde <glalonde@mit.edu>
2014-05-11 08:36:09 -0400

    meh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 alias gt="go test 2>/dev/null | egrep -v 'EOF|connection|broken'"
-sudo apt-get install sqlite3
+#sudo apt-get install sqlite3
