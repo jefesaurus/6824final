4e21fbc22e4bc7e9fe8911d7c6b56193ae86b02b
Geoffrey Lalonde <glalonde@mit.edu>
2014-05-11 23:54:58 -0400

    fixed typo

--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -12,6 +12,10 @@ import "encoding/gob"
 import "math"
 import "math/rand"
 import "time"
+import "dbaccess"
+import "strconv"
+import "strings"
+
 
 type ShardMaster struct {
   mu sync.Mutex
@@ -19,12 +23,14 @@ type ShardMaster struct {
   me int
   dead bool // for testing
   unreliable bool // for testing
+  db *dbaccess.DBClerk
+  db_path string
+
   px *paxos.Paxos
   pxMu sync.Mutex
 
   configs []Config // indexed by config num
   queryReplies map[int]Config // Keep track of replies
-  lastApplied int
   doneWith map[int]bool // Ops that are totally done
 }
 
@@ -63,7 +69,8 @@ func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) error {
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) error {
   thisOp := Op{Opcode: QUERY, Num: args.Num}
   seq := sm.StartAndFinishOp(thisOp)
-  config := sm.queryReplies[seq]
+  //config := sm.queryReplies[seq]
+  config := sm.GetReply(seq)
   reply.Config = config
   return nil
 }
@@ -118,7 +125,6 @@ func (sm *ShardMaster) LogWalker() {
     if decided, opData := sm.px.Status(seq); decided {
       timeout = 10
       sm.ApplyOp(seq, opData.(Op))
-      sm.lastApplied = seq
       seq ++
     } else {
       sm.px.Start(seq, Op{Opcode: QUERY, Num:-1})
@@ -133,25 +139,29 @@ func (sm *ShardMaster) LogWalker() {
 func (sm *ShardMaster) ApplyOp(seq int, op Op) bool {
   sm.mu.Lock()
   defer sm.mu.Unlock()
+  config_num := sm.DBGetNumConfigs()
 
-  if !sm.doneWith[seq] { // We apply it, and calculate the reply if we haven't already seen it
+  if !sm.GetDone(seq) { // We apply it, and calculate the reply if we haven't already seen it
     if op.Opcode == QUERY {
-      if op.Num < 0 || op.Num > len(sm.configs) {
-        sm.queryReplies[seq] = CopyConfig(&sm.configs[len(sm.configs)-1])
+      var rep Config
+      if op.Num < 0 || op.Num > config_num {
+        rep = sm.GetConfig(config_num-1)
       } else {
-        sm.queryReplies[seq] = CopyConfig(&sm.configs[op.Num])
+        rep = sm.GetConfig(op.Num)
       }
+      sm.PutReply(seq, &rep)
     } else {
-      conf := CopyConfig(&sm.configs[len(sm.configs) - 1])
+      conf := sm.GetConfig(config_num - 1)
       conf.Num ++
       switch op.Opcode {
       case JOIN: conf.ApplyJoin(op.GID, op.Servers)
       case LEAVE: conf.ApplyLeave(op.GID)
       case MOVE: conf.ApplyMove(op.Num, op.GID)
       }
-      sm.configs = append(sm.configs, conf)
+      sm.PutConfig(config_num, &conf)
     }
-    sm.doneWith[seq] = true
+    //sm.doneWith[seq] = true
+    sm.SetDone(seq)
     return true
   }
   return false
@@ -210,18 +220,7 @@ func (conf *Config) ApplyLeave(GID int64) {
 }
 
 func (conf *Config) ApplyMove(Shard int, GID int64) {
-  previousGID := conf.Shards[Shard]
   conf.Shards[Shard] = GID
-  isGone := true
-  for _, gid := range conf.Shards {
-    if gid == previousGID {
-      isGone = false
-      break
-    }
-  }
-  if isGone {
-    //delete(conf.Groups, previousGID)
-  }
 }
 
 func CopyConfig(old *Config) Config {
@@ -266,6 +265,118 @@ func ArgRange(gids map[int64][]int) (int64,int64,int,int) {
   return minKey, maxKey, minVal, maxVal
 }
 
+func (sm *ShardMaster) GetConfig(config_num int) Config {
+  //mem_conf := sm.configs[config_num]
+  db_conf := sm.DBGetConfig(config_num)
+  //CheckEq(&mem_conf, db_conf)
+  return *db_conf
+}
+
+func (sm *ShardMaster) PutConfig(config_num int, conf *Config) {
+  //sm.configs = append(sm.configs, *conf)
+  sm.DBPutConfig(config_num, conf)
+  sm.DBIncNumConfigs()
+}
+
+func CheckEq(a *Config, b *Config){
+  if a.Num != b.Num {
+    fmt.Printf("Not the same: %d, %d\n", a.Num, b.Num)
+  }
+  for i, anum := range a.Shards {
+    if b.Shards[i] != anum {
+      fmt.Println("Not the same")
+    }
+  }
+}
+
+func (sm *ShardMaster) PutReply(seq int, conf *Config) {
+  //sm.queryReplies[seq] = *conf
+  sm.DBPutReply(seq, conf)
+}
+
+func (sm *ShardMaster) GetReply(seq int) Config {
+  //mem_conf := sm.queryReplies[seq]
+  db_conf := sm.DBGetReply(seq)
+  //CheckEq(&mem_conf, db_conf)
+  return *db_conf
+}
+
+func (sm *ShardMaster) SetDone(seq int) {
+  sm.doneWith[seq] = true
+  sm.DBSetDone(seq)
+}
+
+func (sm *ShardMaster) GetDone(seq int) bool {
+  mem_done := sm.doneWith[seq]
+  db_done := sm.DBGetDone(seq)
+  if mem_done != db_done {
+    fmt.Println("NOT EQUAL")
+  }
+  return db_done
+}
+
+
+//######################################################################
+const (
+  METADATA = 10
+  CONFIG_TABLE = 11
+  REPLY_TABLE = 12
+  DONE_TABLE = 13
+)
+
+/*
+  configs []Config // indexed by config num
+  queryReplies map[int]Config // Keep track of replies
+  doneWith map[int]bool // Ops that are totally done
+*/
+
+func (sm *ShardMaster) DBGetNumConfigs() int {
+  num_configs, exists := sm.db.GetInt(METADATA, "num_configs")
+  if !exists {
+    log.Fatal("No config count in db")
+  }
+  return num_configs
+}
+func (sm *ShardMaster) DBIncNumConfigs() {
+  num_configs := sm.DBGetNumConfigs()
+  sm.db.PutInt(METADATA, "num_configs", num_configs+1)
+}
+
+func (sm *ShardMaster) DBPutConfig(config_num int, conf *Config) {
+  sm.db.PutStruct(CONFIG_TABLE, strconv.Itoa(config_num), conf)
+}
+
+func (sm *ShardMaster) DBGetConfig(config_num int) *Config {
+  conf := &Config{}
+  sm.db.GetStruct(CONFIG_TABLE, strconv.Itoa(config_num), conf)
+  return conf
+}
+
+func (sm *ShardMaster) DBPutReply(seq int, conf *Config) {
+  sm.db.PutStruct(REPLY_TABLE, strconv.Itoa(seq), conf)
+}
+
+func (sm *ShardMaster) DBGetReply(seq int) *Config {
+  conf := &Config{}
+  sm.db.GetStruct(REPLY_TABLE, strconv.Itoa(seq), conf)
+  return conf
+}
+
+func (sm *ShardMaster) DBSetDone(seq int) {
+  sm.db.PutInt(DONE_TABLE, strconv.Itoa(seq), 1)
+}
+
+func (sm *ShardMaster) DBGetDone(seq int) bool {
+  done_num, exists := sm.db.QuietGetInt(DONE_TABLE, strconv.Itoa(seq))
+  if !exists {
+    return false
+  }
+  return (done_num > 0)
+}
+
+
+//######################################################################
+
 // please don't change this function.
 func (sm *ShardMaster) Kill() {
   sm.dead = true
@@ -273,24 +384,52 @@ func (sm *ShardMaster) Kill() {
   sm.px.Kill()
 }
 
+const db_path = "/tmp/smdb/"
+
+func (sm *ShardMaster) FreshStart(servers []string, me int) {
+  // Put my index
+  sm.db.PutInt(METADATA, "me", me)
+  sm.me = me
+
+  // Enter peers
+  sm.db.PutStringList(METADATA, "servers", servers)
+
+  first_conf := new(Config)
+  first_conf.Groups = map[int64][]string{}
+  sm.DBPutConfig(0, first_conf)
+  sm.db.PutInt(METADATA, "num_configs", 1)
+}
+
+func StartServer(servers []string, me int) *ShardMaster {
+  gob.Register(Op{})
+  sm := new(ShardMaster)
+
+  path_parts := strings.Split(servers[me], "/")
+  sm.db_path = db_path + path_parts[len(path_parts)-1]
+  os.Remove(sm.db_path)
+  sm.db = dbaccess.GetDatabase(sm.db_path)
+
+  sm.FreshStart(servers, me)
+  FinishStartServer(sm, servers, me)
+  return sm
+}
+
+
+
 //
 // servers[] contains the ports of the set of
 // servers that will cooperate via Paxos to
 // form the fault-tolerant shardmaster service.
 // me is the index of the current server in servers[].
 // 
-func StartServer(servers []string, me int) *ShardMaster {
-  gob.Register(Op{})
+func FinishStartServer(sm *ShardMaster, servers []string, me int) *ShardMaster {
 
-  sm := new(ShardMaster)
-  sm.me = me
+  //sm.configs = make([]Config, 1)
+  //sm.configs[0].Groups = map[int64][]string{}
 
-  sm.configs = make([]Config, 1)
-  sm.configs[0].Groups = map[int64][]string{}
+  //sm.queryReplies = make(map[int]Config)
 
-  sm.queryReplies = make(map[int]Config)
   sm.doneWith = make(map[int]bool)
-  sm.lastApplied = -1
 
   rpcs := rpc.NewServer()
   rpcs.Register(sm)
